754942299ca24fdc5db493ca4c52c8bc6afe133e
Vinodhinichokkan <vinodhinichokkan99@gmail.com>
2024-12-18 13:35:20 +0530

    exe

---
+++ classes.py
@@ -0,0 +1,53 @@
+# ----------classes--------
+
+class Vehicle:
+    def __init__(self, make, model):
+        self.make = make
+        self.model = model
+
+    def moves(self):
+        print('Moves along..')  #Moves along..
+
+    def get_make_model(self):     #I'm a Tesla Model Y.
+        print(f"I'm a {self.make} {self.model}.")
+
+my_car = Vehicle('Tesla','Model Y')
+
+my_car.moves()
+print(my_car.make)   #Tesla
+print(my_car.model)  #Model Y
+my_car.get_make_model() 
+
+print('')
+
+#--------------------Inheritance-------------------
+
+class Airplane(Vehicle):
+    def moves(self):
+        print('Rumbles along..')
+      
+class Truck(Vehicle):
+    def moves(self):
+        print('Rumbles along..')
+
+class GolfCart(Vehicle):
+    pass
+
+cessna = Airplane('Cessna','Skywalk')
+mack = Truck('Mack','Pinnacle')
+golfwagon = GolfCart('Yamaha','GC100')
+
+cessna.get_make_model()       #I'm a Cessna Skywalk.
+                              #Rumbles along..
+                              #I'm a Mack Pinnacle.
+                              #Rumbles along..
+                              #I'm a Yamaha GC100.
+                              #Moves along..
+cessna.moves()
+mack.get_make_model()
+mack.moves()
+golfwagon.get_make_model()
+golfwagon.moves()
+
+
+    
--- lambda1.py
+++ lambda1.py
@@ -1,9 +1,23 @@
 numbers = [3, 7, 12, 18, 20, 21]
 
-squared_nums = map(lambda num: num * num,numbers)
+squared_nums = map(lambda num: num * num,numbers)#-------------->map will match the each and every element in the list
 
 print(list(squared_nums))  #[9, 49, 144, 324, 400, 441]
 
-odd_nums= filter(lambda num: num%2 !=0, numbers)
+odd_nums= filter(lambda num: num%2 !=0, numbers)#------------>filter will check whether the given condition is true or false.
 
 print(list(odd_nums))   #[3, 7, 21]
+
+from functools import reduce
+numbers = [1, 2, 3, 4, 5, 1]
+total = reduce(lambda acc, curr: acc  + curr ,numbers)
+print (total)    #16
+
+#---------another method
+print(sum(numbers))  #16
+
+
+names = ['Vinodhini','Abirami','Hari']
+char_count = reduce(lambda acc,curr: acc + len(curr), names, 0)#---->0 is to convert string to number
+print(char_count)  #20
+
